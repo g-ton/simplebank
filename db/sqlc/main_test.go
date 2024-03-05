184590849bad14d88587a714f38c9a29b19ac108
J. Damian <jdamianjm@gmail.com>
2024-03-04 22:39:38 -0600

    Adding routing implementing endpoints to manage accounts and We added viper to control the env vars in the system

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/g-ton/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:kuna1234@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	// ../.. means to go to the root of the project
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load configuration:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
